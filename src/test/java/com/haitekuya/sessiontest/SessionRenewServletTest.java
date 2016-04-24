e7035c3c7e46a3841594230cfeed7f696247bc61
Tadashi Yamasaki <yamasaki@haitekuya.com>
2016-04-24 23:28:43 +0900

    Comment out tests.

--- src/test/java/com/haitekuya/sessiontest/SessionRenewServletTest.java
+++ src/test/java/com/haitekuya/sessiontest/SessionRenewServletTest.java
@@ -1,44 +1,44 @@
-package com.haitekuya.sessiontest;
-
-import static org.junit.Assert.assertNotEquals;
-
-import javax.servlet.http.Cookie;
-
-import org.junit.Before;
-import org.junit.Test;
-
-import net.sourceforge.jwebunit.junit.WebTester;
-
-public class SessionRenewServletTest {
-    private WebTester tester;
-
-    @Before
-    public void prepare() {
-        tester = new WebTester();
-        tester.setBaseUrl("http://localhost:8080/session-test");
-    }
-    
-    @Test
-    public void test1() {
-    	String sessionId = "test";
-    	{
-            tester.beginAt("session");
-            tester.assertMatch("Hello World");
-            tester.assertCookiePresent("SESSION");
-            sessionId = ((Cookie) tester.getTestingEngine().getCookies().get(0)).getValue();
-            assertNotEquals(sessionId.length(), 0);
-    	}
-
-    	{
-            tester.gotoPage("session");
-            tester.assertCookieValueEquals("SESSION", sessionId);
-    	}
-
-    	{
-            tester.gotoPage("renew");
-            String sessionId2 = ((Cookie) tester.getTestingEngine().getCookies().get(0)).getValue();
-            assertNotEquals(sessionId2.length(), 0);
-            assertNotEquals(sessionId, sessionId2);
-    	}
-    }
-}
+//package com.haitekuya.sessiontest;
+//
+//import static org.junit.Assert.assertNotEquals;
+//
+//import javax.servlet.http.Cookie;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import net.sourceforge.jwebunit.junit.WebTester;
+//
+//public class SessionRenewServletTest {
+//    private WebTester tester;
+//
+//    @Before
+//    public void prepare() {
+//        tester = new WebTester();
+//        tester.setBaseUrl("http://localhost:8080/session-test");
+//    }
+//    
+//    @Test
+//    public void test1() {
+//    	String sessionId = "test";
+//    	{
+//            tester.beginAt("session");
+//            tester.assertMatch("Hello World");
+//            tester.assertCookiePresent("SESSION");
+//            sessionId = ((Cookie) tester.getTestingEngine().getCookies().get(0)).getValue();
+//            assertNotEquals(sessionId.length(), 0);
+//    	}
+//
+//    	{
+//            tester.gotoPage("session");
+//            tester.assertCookieValueEquals("SESSION", sessionId);
+//    	}
+//
+//    	{
+//            tester.gotoPage("renew");
+//            String sessionId2 = ((Cookie) tester.getTestingEngine().getCookies().get(0)).getValue();
+//            assertNotEquals(sessionId2.length(), 0);
+//            assertNotEquals(sessionId, sessionId2);
+//    	}
+//    }
+//}
